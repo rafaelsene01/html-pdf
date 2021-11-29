b9057d2ca769d63552786bb6680101ada64baa44
rafaelsene01 <rafaelssene@gmail.com>
2021-11-29 17:22:10 -0300

    feat: conversão para pdf

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { join } from 'path';
-import handlebars from 'handlebars';
 
 import { AppModule } from 'src/module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  app.useStaticAssets({
-    root: join(__dirname, '..', 'src', 'views', 'css'),
-    prefix: '/css/',
-  });
-  app.setViewEngine({
-    engine: {
-      handlebars,
-    },
-    templates: join(__dirname, '..', 'src', 'views'),
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'src', 'views'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
+  app.setViewEngine('hbs');
 
   await app.listen(5000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
-
 bootstrap();
